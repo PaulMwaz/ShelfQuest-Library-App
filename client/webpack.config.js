fe019b2075fd83353d7afecc5941153214493c3a
Paul <paul.kyalo@student.moringaschool.com>
2025-03-25 09:34:03 +0300

    Updates to render on cloudflare

---
+++ client/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"), // Output to 'build' for Cloudflare
+    filename: "bundle.js",
+    publicPath: "/", // Important for SPA routing on Cloudflare Pages
+    clean: true,
+    assetModuleFilename: "assets/images/[name][ext]",
+  },
+  mode: "production",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"), // Corrected to 'build'
+    },
+    port: 4000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+  resolve: {
+    extensions: [".js"], // Only JavaScript
+  },
+  devtool: "source-map",
+};
